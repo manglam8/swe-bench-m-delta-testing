8f5006c5d0d06125bec1331457aebed71d5ba0c2
Kumar Manglam Pandey <manglam8@hotmail.com>
2026-07-02 22:17:05 +0000

    add all evaluators to interface

--- src/sbmdt/interface.py
+++ src/sbmdt/interface.py
@@ -11,6 +11,9 @@ from __future__ import annotations
 
 from sbmdt.evaluator.alibaba import AlibabaEvaluator
 from sbmdt.evaluator.base import PatchType, TestResult
+from sbmdt.evaluator.grommet import GrommetEvaluator
+from sbmdt.evaluator.lighthouse import LighthouseEvaluator
+from sbmdt.evaluator.prettier import PrettierEvaluator
 from sbmdt.pred import Pred
 
 __all__ = ['evaluate']
@@ -48,8 +51,21 @@ def evaluate(
             agent_name=Pred.get_agent_name(pred),
             pred=pred,
         )
+    elif instance_id.startswith('grommet'):
+        evaluator = GrommetEvaluator(
+            instance_id=instance_id,
+            patch_type=patch_type,
+            agent_name=Pred.get_agent_name(pred),
+            pred=pred,
+        )
+    elif instance_id.startswith('GoogleChrome'):
+        evaluator = LighthouseEvaluator(
+            instance_id=instance_id,
+            patch_type=patch_type,
+            agent_name=Pred.get_agent_name(pred),
+            pred=pred,
+        )
     elif instance_id.startswith('prettier'):
-        from sbmdt.evaluator.prettier import PrettierEvaluator
         evaluator = PrettierEvaluator(
             instance_id=instance_id,
             patch_type=patch_type,
